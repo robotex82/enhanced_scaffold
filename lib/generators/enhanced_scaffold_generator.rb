8d4ff0822d66f53643d9a9cb8b389efd4e2ef3c7
Roberto Vasquez Angel <robotex@robotex.de>
2015-07-22 13:03:05 +0200

    Added templates.

--- lib/generators/enhanced_scaffold_generator.rb
+++ lib/generators/enhanced_scaffold_generator.rb
@@ -1,4 +1,5 @@
 require 'rails/generators/erb/scaffold/scaffold_generator'
 
 class EnhancedScaffoldGenerator < Erb::Generators::ScaffoldGenerator
+  source_root File.expand_path('../templates', __FILE__)
 end
