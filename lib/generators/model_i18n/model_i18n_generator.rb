0d752f7f4f4362e88b74a97007cd3d05d8437650
Roberto Vasquez Angel <robotex@robotex.de>
2015-07-27 01:30:45 +0200

    Added locale support.

--- lib/generators/model_i18n/model_i18n_generator.rb
+++ lib/generators/model_i18n/model_i18n_generator.rb
@@ -7,20 +7,30 @@ module EnhancedScaffold
       source_root File.expand_path('../templates', __FILE__)
 
       argument :attributes, type: :array, default: [], banner: "field[:type][:index] field[:type][:index]"
+      class_option :locales, type: :array, default: [:en], desc: "en de"
       # hook_for :orm, required: true, desc: "ORM to be invoked"
 
       def generate_locale_files
-        I18n.available_locales.each do |locale|
+        enforce = I18n.enforce_available_locales
+        I18n.enforce_available_locales = false
+
+        locales.each do |locale|
           I18n.with_locale(locale) do
-            template 'locale.yml', File.join("config", "locales", "#{file_name}.#{locale}.yml")
+            template 'locale.yml', File.join("config", "locales", "#{file_name}.#{locale.to_s}.yml")
           end
         end
+
+        I18n.enforce_available_locales = enforce
       end
 
       private
 
       def locale
         I18n.locale
+      end
+
+      def locales
+        options[:locales]
       end
     end
   end
